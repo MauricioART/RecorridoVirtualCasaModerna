481e547363d70e420659da5669455741e2242460
artuaguila@gmail.com <artuaguila@gmail.com>
2021-07-03 14:24:49 -0500

    Código Base para el proyecto

---
+++ RecorridoVirtualCasaModerna/RecorridoVirtualCasaModerna/Window.h
@@ -0,0 +1,48 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getKittX() { return kittX; }
+	GLfloat getKittZ() { return kittZ; }
+	GLfloat getBHx() { return BHx; }
+	GLfloat getBHy() { return BHy; }
+	GLfloat getBHz() { return BHz; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat kittX;
+	GLfloat kittZ;
+	GLfloat BHx;
+	GLfloat BHy;
+	GLfloat BHz;
+
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
